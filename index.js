444ae90ea62023464ecf85ae84b6f83aae1cbd84
Bridget Kelly <bckelly197@gmail.com>
2022-02-06 15:55:03 -0500

    commit tree d587c95b883ef60982b6cdf58cfbbefed98ec371

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 // Your code here
+// const dodger = document.getElementById("dodger");
+// function moveDodgerLeft() {
+//   const leftNumbers = dodger.style.left.replace("px", "");
+//   const left = parseInt(leftNumbers, 10);
+
+//   if (left > 0) {
+//     dodger.style.left = `${left - 1}px`;
+//   }
+// }
+// document.addEventListener("keydown", function (e) {
+//     if (e.key === "ArrowLeft") {
+//       moveDodgerLeft()     
+//     }
+// });
+
+// function moveDodgerRight() {
+//     const leftNumbers = dodger.style.left.replace("px", "");
+//     const left = parseInt(leftNumbers, 10);
+  
+//     if (left < 360) {
+//       dodger.style.left = `${left + 1}px`;
+//     }
+//   }
+  
+// document.addEventListener("keydown", function (e) {
+//     if (e.key === "ArrowRight") {
+//       moveDodgerRight()      
+//     }
+// });
+
+
+
+
+
+
+
+
+
 const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+
 function moveDodgerLeft() {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
@@ -8,23 +49,33 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`;
   }
 }
+
 document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft()     
-    }
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
 });
 
 function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left < 360) {
-      dodger.style.left = `${left + 1}px`;
-    }
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
   }
-  
+}
+
 document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowRight") {
-      moveDodgerRight()      
-    }
+  if (e.key === "ArrowRight") {
+  moveDodgerRight();
+  }
 });
+
+
+
+
+
+
+
+
+
